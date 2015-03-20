08fbaa5cfe7fefb1672bfa25acc999e2e72771d4
Jakub Jedryszek <jakub.jedryszek@gmail.com>
2015-03-20 00:04:00 -0700

    fixed styles + added less compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
+var path = require('path');
 var serverPath = './server/**/*.js';
 var appPath = './public/app/**/*.js';
+var stylesheetsPath = './public/stylesheets/**/*.less';
+
+// jshint linting
+
+gulp.task('lint', ['lint-server', 'lint-app']);
 
 gulp.task('lint-server', function () {
   return gulp.src(serverPath)
@@ -18,11 +26,22 @@ gulp.task('lint-app', function () {
           .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('lint', ['lint-server', 'lint-app']);
+// styles
+
+gulp.task('less', function () {
+  return gulp.src(stylesheetsPath)
+          .pipe(less({paths: [ path.join(__dirname, 'less', 'includes') ]}))
+          .pipe(minifyCSS())
+          .pipe(gulp.dest('./public/stylesheets'));
+});
+
+
+// core
 
 gulp.task('watch', function () {
   gulp.watch(serverPath, ['lint-server']);
   gulp.watch(appPath, ['lint-app']);
+  gulp.watch(stylesheetsPath, ['less']);
 });
 
-gulp.task('default', ['lint', 'watch']);
+gulp.task('default', ['lint', 'less', 'watch']);
